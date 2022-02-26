2df5b0b9611398a45db3bf35f1570def77e8c9af
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-25 19:38:27 -0800

    finished lab 10

---
+++ modules/weather.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const axios = require('axios');
+let cache = require('./cache');
+
+function getWeather(lat, lon) {
+  const key = 'weather-' + lat + lon;
+  const weatherUrl = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`;
+
+  let oneDay = 1000 * 60 * 60 *24;
+
+  if (cache[key] && (Date.now() - cache[key].timestamp < oneDay)) {
+    console.log('Cache Hit, What\'s the Weather Like?');
+  } else {
+    console.log('Cache Miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(weatherUrl)
+      .then(response => parseWeather(response.data));
+  }
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Weather(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+class Weather {
+  constructor(day) {
+    this.date = day.datetime;
+    this.description = `Weather Forecast: ${day.weather.description}`;
+  }
+}
+
+module.exports = getWeather;
--- server.js
+++ server.js
@@ -4,28 +4,32 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
+
+const getMovies = require('./modules/movies');
+const getWeather = require('./modules/weather');
 
 const app = express();
-
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
-
-const getMovies = require('./movies');
-const getWeather = require('./weather');
 
 app.get('/', (request, response) => {
   response.status(200).send('King Snorlax Approves');
 });
 
-app.get('/weather', getWeather);
-
+app.get('/weather', weatherHandler);
 app.get('/movie', getMovies);
 
-// app.get('*', (request, response) => {
-//   response.send('Snorlax rules');
-// });
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  getWeather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.log(error);
+      response.status(200).send('Sorry about that, something went wrong.');
+    });
+
+}
 
 
 
